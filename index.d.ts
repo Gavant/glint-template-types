fde58e6d88d5c4a131ca8feaf4180721e5d45b68
Adam Baker <bakerac4@gmail.com>
2021-04-14 13:56:55 -0400

    Adding FaIcon class

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+import { FaIconComponent } from '@gavant/glint-template-types/types/@fortawesome/ember-fontawesome/fa-icon';
 import { DidInsertModifier } from '@gavant/glint-template-types/types/ember-render-modifiers/did-insert';
 import { DidUpdateModifier } from '@gavant/glint-template-types/types/ember-render-modifiers/did-update';
 
@@ -5,5 +6,6 @@ declare module '@glint/environment-ember-loose/registry' {
     export default interface Registry {
         'did-insert': typeof DidInsertModifier;
         'did-update': typeof DidUpdateModifier;
+        FaIcon: typeof FaIconComponent;
     }
 }
