7ccd0b14e3a31af36fa6501b0624e11248e67554
Peter Wagenet <peter.wagenet@gmail.com>
2021-04-21 08:20:04 -0700

    Add ember-on-helper

---
+++ types/ember-on-helper/on-document.d.ts
@@ -0,0 +1,12 @@
+import Helper from "@glint/environment-ember-loose/ember-component/helper";
+import { OnArgs } from "./-private/shared";
+
+interface OnDocumentHelperSignature<K extends keyof DocumentEventMap> {
+  Args: OnArgs;
+  PositionalArgs: [eventName: K, handler: (event: DocumentEventMap[K]) => void];
+  Return: null;
+}
+
+export default class OnDocumentHelper<K extends keyof DocumentEventMap> extends Helper<
+  OnDocumentHelperSignature<K>
+> {}
