36fc5621a14386465f4459d0040e83e7e4e42858
Peter Wagenet <peter.wagenet@gmail.com>
2021-04-21 07:54:08 -0700

    Add types for ember-cli-clipboard

---
+++ types/ember-cli-clipboard/copy-button.d.ts
@@ -0,0 +1,14 @@
+import Component from "@glint/environment-ember-loose/ember-component";
+
+interface CopyButtonSignature {
+  Element: HTMLButtonElement;
+  Args: {
+    clipboardText: string;
+    success: () => void;
+  };
+  Yields: {
+    default: [];
+  };
+}
+
+export default class CopyButton extends Component<CopyButtonSignature> {}
---
+++ types/ember-cli-clipboard/is-clipboard-supported.d.ts
@@ -0,0 +1,7 @@
+import Helper from "@glint/environment-ember-loose/ember-component/helper";
+
+interface IsClipboardSupportedSignature {
+  Return: boolean;
+}
+
+export default class IsClipboardSupported extends Helper<IsClipboardSupportedSignature> {}
