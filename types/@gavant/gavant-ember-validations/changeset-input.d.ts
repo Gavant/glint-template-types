79fff63035e895c9cb0e4da24b68c2b095e7a484
Adam Baker <bakerac4@gmail.com>
2021-05-03 14:38:22 -0400

    Fixing changeset input type

--- types/@gavant/gavant-ember-validations/changeset-input.d.ts
+++ types/@gavant/gavant-ember-validations/changeset-input.d.ts
@@ -3,7 +3,7 @@ import Component from '@glint/environment-ember-loose/ember-component';
 import { GenericChangeset } from '@gavant/ember-validations/utilities/create-changeset';
 
 export interface ChangesetInputSignature<T> {
-    Element: HTMLElement;
+    Element: HTMLInputElement;
     Args: {
         changeset: GenericChangeset<T>;
         path: keyof T;
