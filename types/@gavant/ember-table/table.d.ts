df0d9c6ad3752b5c1ab5fef636a0b5ce496979f6
Adam Baker <bakerac4@gmail.com>
2021-05-05 11:35:50 -0400

    Adding in types for gavant-ember-table

---
+++ types/@gavant/ember-table/table.d.ts
@@ -0,0 +1,6 @@
+import Component from '@glint/environment-ember-loose/ember-component';
+
+import { TableArgs } from '@gavant/ember-table/addon/components/table';
+import { BaseGlimmerSignature } from '@gavant/glint-template-types/utils/types';
+
+export default class Table extends Component<BaseGlimmerSignature<TableArgs>> {}
