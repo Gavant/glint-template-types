bd285dacb2e68c10d93022c276cf841f5afaae82
Adam Baker <bakerac4@gmail.com>
2021-05-10 14:59:18 -0400

    Upgrade types to latest table

--- types/@gavant/ember-table/table.d.ts
+++ types/@gavant/ember-table/table.d.ts
@@ -3,4 +3,4 @@ import Component from '@glint/environment-ember-loose/ember-component';
 import { TableArgs } from '@gavant/ember-table/addon/components/table';
 import { BaseGlimmerSignature } from '@gavant/glint-template-types/utils/types';
 
-export default class Table extends Component<BaseGlimmerSignature<TableArgs>> {}
+export default class Table<R, F> extends Component<BaseGlimmerSignature<TableArgs<R, F>>> {}
