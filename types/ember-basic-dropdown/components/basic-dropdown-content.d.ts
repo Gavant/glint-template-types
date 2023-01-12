0b6eb6d18b1f2387f1274aa3aa210822eae38d11
Adam Baker <bakerac4@gmail.com>
2023-01-12 10:52:03 -0500

    Fixing issues with basic dropdown content and render modifiers

--- types/ember-basic-dropdown/components/basic-dropdown-content.d.ts
+++ types/ember-basic-dropdown/components/basic-dropdown-content.d.ts
@@ -1,9 +1,9 @@
 declare module 'ember-basic-dropdown/components/basic-dropdown-content' {
-    import Component from '@glimmer/component';
+import Component from '@glimmer/component';
 
-    import { Dropdown, RepositionChanges } from 'ember-basic-dropdown/components/basic-dropdown';
+import { Dropdown, RepositionChanges } from 'ember-basic-dropdown/components/basic-dropdown';
 
-    interface Args {
+        interface Args {
         transitioningInClass?: string;
         transitionedInClass?: string;
         transitioningOutClass?: string;
@@ -23,7 +23,7 @@ declare module 'ember-basic-dropdown/components/basic-dropdown-content' {
         onFocusOut?: (dropdown?: Dropdown, event?: FocusEvent) => void;
         onMouseEnter?: (dropdown?: Dropdown, event?: MouseEvent) => void;
         onMouseLeave?: (dropdown?: Dropdown, event?: MouseEvent) => void;
-        shouldReposition: (mutations: MutationRecord[], dropdown: Dropdown) => boolean;
+        shouldReposition?: (mutations: MutationRecord[], dropdown: Dropdown) => boolean;
         hPosition: RepositionChanges['hPosition'];
         vPosition: RepositionChanges['vPosition'];
     }
--- types/ember-render-modifiers/will-destroy.d.ts
+++ types/ember-render-modifiers/will-destroy.d.ts
@@ -1,6 +1,6 @@
-import { RenderModifier as WillDestroyModifier } from './-private/shared';
-
 declare module 'ember-render-modifiers/modifiers/will-destroy' {
+    import { RenderModifier as WillDestroyModifier } from '@gavant/glint-template-types/types/ember-render-modifiers/-private/shared';
+
     export { WillDestroyModifier };
     export default WillDestroyModifier;
 }
