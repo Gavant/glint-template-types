88e675caa5d5cbacc62e16133ae233e0b06b7b54
Charles Fries <charliefries@icloud.com>
2023-05-30 15:51:18 -0700

    Cleanup

--- types/ember-composable-helpers/helpers/call.d.ts
+++ types/ember-composable-helpers/helpers/call.d.ts
@@ -1,9 +1,7 @@
 declare module 'ember-composable-helpers/helpers/call' {
   import Helper from '@ember/component/helper';
 
-  import { AnyFunction } from '@glint/template/-private/integration';
-
-  export default class CallHelper<T extends AnyFunction> extends Helper<{
+  export default class CallHelper<T extends (...args: any) => any> extends Helper<{
     Args: {
       Positional: [fn: T];
     };
